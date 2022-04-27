7341f9ac33fb40f3aaacf76a91ffa86d333192be
Snoiman0047 <Snoiman0047@gmail.com>
2022-04-28 01:56:47 +0300

    upload userbase service.

---
+++ userbase/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  development: {
+    ports: {
+      portal: 3100,
+      authentication: 3200,
+      accounts: 3400,
+      transactions: 3600,
+      bills: 3800,
+      support: 4000,
+      userbase: 4100,
+      anomaly_detection: 4200
+    }
+  },
+  production: {
+    ports: {
+      portal: 30200,
+      authentication: 30100,
+      accounts: 30120,
+      transactions: 30140,
+      bills: 30160,
+      support: 30180,
+      userbase: 30050,
+      anomaly_detection: 30170
+    }
+  }
+};
